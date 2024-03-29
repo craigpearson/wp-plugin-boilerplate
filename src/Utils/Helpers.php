f20e617b8c1e93a71d430bce15df4f136cd44a78
Craig Pearson <hello@craigpearson.co.uk>
2024-03-29 20:23:44 +0000

    Better helpers

--- src/Utils/Helpers.php
+++ src/Utils/Helpers.php
@@ -13,10 +13,23 @@ namespace WpPluginMold\Utils;
  * @package WpPluginMold
  */
 class Helpers {
+
+	/** The version for the plugin */
+	const PLUGIN_VERSION = '1.0.0';
+
+	/** The Text domain for the plugin */
+	const TEXT_DOMAIN = 'wp-plugin-mold';
+
+	/** The file for the plugin */
+	const PLUGIN_FILE = 'wp-plugin-mold.php';
+
+	/** The path for the plugin */
+	const PLUGIN_PATH = __DIR__ . '/../../';
+
 	/**
 	 * Main method for the service, called by the container.
 	 */
 	public function boot(): void {
-		// The start of your helpers
+
 	}
 }
