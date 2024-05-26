7ebb92e7761c04aa9682ec821db6e735634e456a
Craig Pearson <hello@craigpearson.co.uk>
2024-05-26 16:38:00 +0100

    Custom service container

---
+++ src/Exception/ContainerNotFoundException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Exception thrown if a service is not found in the container.
+ *
+ * @package WpPluginMold\Exception
+ */
+
+namespace WpPluginMold\Exception;
+
+use Exception;
+
+/**
+ * Exception thrown if a service is not found in the container.
+ *
+ * @package WpPluginMold\Exception
+ */
+class ContainerNotFoundException extends Exception {}
