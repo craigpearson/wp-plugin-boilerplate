3253c24ee7384311ea1c8752d97366352449e8c1
Craig Pearson <hello@craigpearson.co.uk>
2024-05-26 16:36:35 +0100

    Linting

--- bin/defaults.php
+++ bin/defaults.php
@@ -1,22 +1,24 @@
 <?php
-// Basic WordPress Plugin Meta
-$plugin_name = 'WP Plugin Mold';
-$plugin_uri = 'https://example.com/wp-plugin-mold';
-$plugin_description = 'A mold for creating WordPress plugins.';
-$plugin_version = '1.0.0-alpha';
-$plugin_requires_wp_at_least = '6.4.3';
-$plugin_requires_php = '8.0';
-$plugin_author = 'Your Name';
-$plugin_author_uri = 'https://example.com';
-$plugin_license = 'GPL-3.0-or-later';
-$plugin_license_uri = 'https://www.gnu.org/licenses/gpl-3.0.html';
-$plugin_update_uri = 'https://example.com/wp-plugin-mold-update';
-$plugin_text_domain = 'wp-plugin-mold';
-$plugin_domain_path = '/languages';
+/**
+ * Default values for the plugin setup.
+ *
+ * @package WpPluginMold
+ */
 
-// Broad PHP Settings
-$namespace = 'WpPluginMold';
-
-// Composer Specific Settings
-$author_email = '';
-
+return [
+    'plugin_name' => 'WP Plugin Mold',
+    'plugin_uri' => 'https://example.com/wp-plugin-mold',
+    'plugin_description' => 'A mold for creating WordPress plugins.',
+    'plugin_version' => '1.0.0',
+    'plugin_requires_wp_at_least' => '6.4.3',
+    'plugin_requires_php' => '8.0',
+    'plugin_author' => 'Your Name',
+    'plugin_author_uri' => 'https://example.com',
+    'plugin_license' => 'GPL-3.0-or-later',
+    'plugin_license_uri' => 'https://www.gnu.org/licenses/gpl-3.0.html',
+    'plugin_update_uri' => 'https://example.com/wp-plugin-mold-update',
+    'plugin_text_domain' => 'wp-plugin-mold',
+    'plugin_domain_path' => '/languages',
+    'namespace' => 'WpPluginMold',
+    'author_email' => 'example@example.com'
+];
